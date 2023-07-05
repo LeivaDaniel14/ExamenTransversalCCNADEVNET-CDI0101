1d29de73fa89bcb08e5283b888e8ebd75daab316
LeivaDaniel14 <d.leivac@duocuc.cl>
2023-07-05 21:03:12 +0000

    Examen DEVNET

---
+++ integrantes.py
@@ -0,0 +1,3 @@
+integrante1 = input("¿Igresa el nombre y apellido del primer del integrante?")
+inetgrante2 = input("¿Ingresa el nombre y apellido del segundo integrante?")
+print("Hola " + integrante1 + " y " + inetgrante2 + " ¡Bienvenido al Examen de CCNA DEVNET!")
